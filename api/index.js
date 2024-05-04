106dac4de57bc89c6da9ca7b871755ac4822b1a1
Rijans Bhagat <82694741+riju951@users.noreply.github.com>
2024-05-04 11:11:28 +0530

    changes in index.js

--- api/index.js
+++ api/index.js
@@ -20,10 +20,10 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+// app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.use(express.json());
